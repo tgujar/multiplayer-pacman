8181a0961611f0330aff164aa88910d6511d3833
denncc <denn_c@outlook.com>
2023-03-22 21:33:37 -0700

    now project is properly set up

---
+++ src/main/java/denncc/com/multiplayerPacman/HelloWorldController.java
@@ -0,0 +1,13 @@
+package denncc.com.multiplayerPacman;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController
+{
+    @RequestMapping("/")
+    public String hello()
+    {
+        return "Hello User";
+    }
+}
