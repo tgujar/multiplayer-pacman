5fd289e8328504da853b2ea672c096acb79954f8
denncc <denn_c@outlook.com>
2023-03-22 20:52:11 -0700

    initialize spring boost project but cannot run yet

---
+++ src/main/java/denncc/com/multiplayerPacman/MultiplayerPacmanApplication.java
@@ -0,0 +1,19 @@
+package denncc.com.multiplayerPacman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplayerPacmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplayerPacmanApplication.class, args);
+	}
+
+}
+
+@RestController
+public class HelloController  {
+	@GetMapping("/")
+	public String index
+}
---
+++ src/test/java/denncc/com/multiplayerPacman/MultiplayerPacmanApplicationTests.java
@@ -0,0 +1,13 @@
+package denncc.com.multiplayerPacman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplayerPacmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
